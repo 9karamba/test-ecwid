520bda4ade37d777e4825edc39e9c70b25f27229
9karamba <nas_bel@list.ru>
2021-12-09 17:20:54 +0300

    add init

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 
+EcwidApp.init({
+    app_id: "cstmz-69350256-test",
+});
+
 Ecwid.OnAPILoaded.add(function(page) {
     // var currencyFormat = Ecwid.formatCurrency(12.99);
     console.log('Ecwid', Ecwid);
