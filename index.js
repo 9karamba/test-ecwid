de5c7511e57164cc11e2113fa1186237707b076e
9karamba <nas_bel@list.ru>
2021-12-09 18:11:56 +0300

    test Ecwid js

--- index.js
+++ index.js
@@ -1,21 +1,8 @@
-EcwidApp.init({
-    app_id: "cstmz-69350256-test",
-    autoloadedflag: true,
-    autoheight: true
-});
-
-// Get store info
-var storeData = EcwidApp.getPayload();
-var storeId = storeData.store_id;
-var accessToken = storeData.access_token;
-var lang = storeData.lang;
-
-window.localStorage.setItem("show_ec_logs", "true");
-
-EcwidApp.OnAPILoaded.add(function(page) {
+console.log('test')
+Ecwid.OnAPILoaded.add(function(page) {
     var priceDiv = document.querySelector('.product-details__product-price');
     var price = parseInt(priceDiv.getAttribute("content"))*100;
-    var currencyFormat = EcwidApp.formatCurrency(price);
-    console.log(currencyFormat);
+    var currencyFormat = Ecwid.formatCurrency(price);
+    alert(currencyFormat);
     priceDiv.querySelector('.details-product-price__value').innerHTML = currencyFormat;
 });
